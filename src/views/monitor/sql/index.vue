37283dc1f5d66ff52030d46629d3caed17b86de4
Dimple <bianxiaofeng@sohu.com>
2021-08-18 15:54:49 +0800

    Now DimpleBlog has separate front and backend. This project is used for DimpleBlog backend page.

---
+++ src/views/monitor/sql/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <elFrame :src="sqlApi" />
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import elFrame from '@/components/Iframe/index'
+
+export default {
+  name: 'Sql',
+  components: { elFrame },
+  computed: {
+    ...mapGetters([
+      'sqlApi'
+    ])
+  }
+}
+</script>
---
+++ src/views/tools/swagger/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <elFrame :src="swaggerApi" />
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import elFrame from '@/components/Iframe/index'
+
+export default {
+  name: 'Swagger',
+  components: { elFrame },
+  computed: {
+    ...mapGetters([
+      'swaggerApi'
+    ])
+  }
+}
+</script>
